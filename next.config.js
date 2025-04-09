5499a99cda9176f5b6accc63f4fa84c2e294e06d
Kshitiz Kumar Maurya <kshitizkumarmaurya@MacBook-Pro.local>
2025-04-10 01:57:35 +0530

    d

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     unoptimized: true,
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'flyola.in'],
   },
   env: {
     JWT_SECRET: process.env.JWT_SECRET, // ensure your .env file defines JWT_SECRET
