f16393a00aed4c4a986ca9d189c2964ae6c26053
Kshitiz Kumar Maurya <kshitizkumarmaurya@MacBook-Pro.local>
2025-04-14 13:22:18 +0530

    j

---
+++ src/components/ClientLayout.jsx
@@ -0,0 +1,22 @@
+// components/ClientLayout.jsx
+"use client";
+
+import { usePathname } from "next/navigation";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { AuthProvider } from "@/components/AuthContext";
+
+export default function ClientLayout({ children }) {
+  const pathname = usePathname();
+  const isDashboard =
+    pathname.startsWith("/admin-dashboard") ||
+    pathname.startsWith("/user-dashboard");
+
+  return (
+    <AuthProvider>
+      {!isDashboard && <Header className="mb-10" />}
+      {children}
+      {!isDashboard && <Footer />}
+    </AuthProvider>
+  );
+}
