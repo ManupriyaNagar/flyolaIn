11a048a8423d18e16d23ed3c1ab6fbc2fe2c59c8
Kshitiz Maurya <kshitizmaurya@Kshitizs-MacBook-Pro.local>
2025-08-02 15:42:58 +0530

    D

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <- critical for static HTML export
+  trailingSlash: true,
   images: {
     unoptimized: true,
     domains: ['images.unsplash.com', 'flyola.in'],
   },
   env: {
-    JWT_SECRET: process.env.JWT_SECRET, // for build-time only (won't be in browser)
+    JWT_SECRET: process.env.JWT_SECRET,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
